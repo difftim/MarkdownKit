fd68d390fff43105b5bf891d0c503cbe400c901a
kris.dev000 <kris.dev000@gmail.com>
2022-05-27 21:08:39 +0800

    code font and color

--- MarkdownKit/Sources/UIKit/Elements/Code/MarkdownCode+UIKit.swift
+++ MarkdownKit/Sources/UIKit/Elements/Code/MarkdownCode+UIKit.swift
@@ -11,9 +11,9 @@
 import UIKit
 
 public extension MarkdownCode {
-  static let defaultHighlightColor = UIColor(red: 0.90, green: 0.20, blue: 0.40, alpha: 1.0)
-  static let defaultBackgroundColor = UIColor(red: 0.85, green: 0.85, blue: 0.85, alpha: 1.0)
-  static let defaultFont = UIFont(name: "Menlo-Regular", size: 16)
+    static let defaultHighlightColor = UIColor(red: 58/255.0, green: 129/255.0, blue: 84/255.0, alpha: 1.0)
+  static let defaultBackgroundColor = UIColor(red: 51/255.0, green: 51/255.0, blue: 51/255.0, alpha: 1.0)
+  static let defaultFont = UIFont(name: "Menlo-Regular", size: 12)
 }
 
 #endif
