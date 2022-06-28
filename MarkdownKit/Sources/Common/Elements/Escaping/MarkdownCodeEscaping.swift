88b609322acfc7f2cf7ee49cbdc558d07b004cb1
keith-s <keith.s@fake.com>
2022-06-28 18:11:09 +0800

    match range for 2...4

--- MarkdownKit/Sources/Common/Elements/Escaping/MarkdownCodeEscaping.swift
+++ MarkdownKit/Sources/Common/Elements/Escaping/MarkdownCodeEscaping.swift
@@ -21,17 +21,19 @@ open class MarkdownCodeEscaping: MarkdownElement {
   }
 
   open func match(_ match: NSTextCheckingResult, attributedString: NSMutableAttributedString) {
-    let range = match.range(at: 3)
+    let range = NSRange(
+      location: match.range(at: 2).location,
+      length: (2 ... 4).reduce(0) { $0 + match.range(at: $1).length }
+    )
     // escaping all characters
     let matchString = attributedString.attributedSubstring(from: range).string
 
     // 仅处理**含换行符**
     guard matchString.contains(where: { $0 == "\n" }) else { return }
 
-
     attributedString.replaceCharacters(
-      in: .init(location: match.range(at: 2).location, length: (2...4).reduce(0, { $0 + match.range(at: $1).length })),
-      with: "[View Code](\(MarkdownCodeEscaping.url(code: "```\(matchString)```")))"
+      in: range,
+      with: "[View Code](\(MarkdownCodeEscaping.url(code: matchString)))"
     )
   }
 
@@ -46,7 +48,7 @@ public extension MarkdownCodeEscaping {
     let data = encode(code: code)
     return "http://\(Config.host)?\(data)"
   }
-  public static func code(url: URL) -> String? {
+  static func code(url: URL) -> String? {
     guard let host = url.host, host == Config.host else { return nil }
     guard let data = url.query else { return nil }
     return decode(data: data)
