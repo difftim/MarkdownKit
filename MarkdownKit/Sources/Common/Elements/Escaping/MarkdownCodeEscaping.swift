673e2ac0558f0c920d5dc7d6d7b67a7fb8b9feb9
jone <jone4317@163.com>
2022-07-05 18:52:39 +0800

    ▶ View Code Block

--- MarkdownKit/Sources/Common/Elements/Escaping/MarkdownCodeEscaping.swift
+++ MarkdownKit/Sources/Common/Elements/Escaping/MarkdownCodeEscaping.swift
@@ -36,7 +36,7 @@ open class MarkdownCodeEscaping: MarkdownElement {
       
     attributedString.replaceCharacters(
       in: range,
-      with: "[->View Code](\(MarkdownCodeEscaping.url(lang: language, code: matchString)))"
+      with: "[▶ View Code Block](\(MarkdownCodeEscaping.url(lang: language, code: matchString)))"
     )
   }
     
